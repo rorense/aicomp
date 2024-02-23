0ed79547af8b12291d13bc2a69c866e6fcfb9f3a
rorense <rkorense@gmail.com>
2024-02-23 15:27:58 +1300

    Implemented Companion model to prisma model

---
+++ ai-comp/app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -0,0 +1,20 @@
+import prismadb from "@/lib/prismadb";
+import CompanionForm from "./components/companion-form";
+
+interface CompanionPageProps {
+  params: { companionId: string };
+}
+
+const CompanionPage = async ({ params }: CompanionPageProps) => {
+  const companion = await prismadb.companion.findUnique({
+    where: {
+      id: params.companionId,
+    },
+  });
+
+  const categories = await prismadb.category.findMany();
+
+  return <CompanionForm initialData={companion} categories={categories} />;
+};
+
+export default CompanionPage;
