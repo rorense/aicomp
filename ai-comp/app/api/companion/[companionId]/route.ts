fb8323f6298d0826b04f06cee89b43c34870756a
rorense <rkorense@gmail.com>
2024-03-04 19:54:05 +1300

    Companion Patch route

---
+++ ai-comp/app/api/companion/[companionId]/route.ts
@@ -0,0 +1,52 @@
+import prismadb from "@/lib/prismadb";
+import { currentUser } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+// Patch request incase the user wants to update the companion
+export async function PATCH(
+  req: Request,
+  { params }: { params: { companionId: string } }
+) {
+  try {
+    const body = await req.json();
+    const user = await currentUser();
+    const { src, name, description, instruction, seed, categoryId } = body;
+
+    if (!params.companionId) {
+      return new NextResponse("Companion ID is required", { status: 400 });
+    }
+
+    // if user does not exist, return unauthorized
+    if (!user || !user.id || !user.firstName) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    // Check if body has required fields
+    if (!src || !name || !description || !instruction || !categoryId) {
+      return new NextResponse("missing Required fields", { status: 400 });
+    }
+
+    // Check for subscriptions
+
+    const companion = await prismadb.companion.update({
+      where: {
+        id: params.companionId,
+      },
+      data: {
+        categoryId,
+        userId: user.id,
+        userName: user.firstName,
+        src,
+        name,
+        description,
+        instruction,
+        seed,
+      },
+    });
+
+    return NextResponse.json(companion);
+  } catch (error) {
+    console.log("[COMPANION_PATCH], error");
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
