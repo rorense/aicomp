93adab012cc3a4a265ee1082e697d23938cf70b3
rorense <rkorense@gmail.com>
2024-03-05 10:58:02 +1300

    Chat client implemented

---
+++ ai-comp/app/(chat)/(routes)/chat/[chatId]/components/chat-header.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Companion, Message } from "@prisma/client";
+import { ChevronLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+interface ChatHeaderProps {
+  companion: Companion & {
+    messages: Message[];
+    _count: {
+      messages: number;
+    };
+  };
+}
+
+const ChatHeader = ({ companion }: ChatHeaderProps) => {
+  const router = useRouter();
+
+  return (
+    <div className="flex w-full justify-between items-center border-b border-primary/10 pb-4">
+      <div className="flex gap-x-2 items-center">
+        <Button size="icon" variant="ghost" onClick={() => router.back()}>
+          <ChevronLeft className="h-8 w-8" />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default ChatHeader;
