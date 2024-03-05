ad4732f259c19020aeee77dfba5c95367627e115
rorense <rkorense@gmail.com>
2024-03-06 08:49:34 +1300

    Implemented chat UI featuires

--- ai-comp/app/(chat)/(routes)/chat/[chatId]/components/chat-message.tsx
+++ ai-comp/app/(chat)/(routes)/chat/[chatId]/components/chat-message.tsx
@@ -16,6 +16,7 @@ export interface ChatMessageProps {
   src?: string;
 }
 
+// The actual chat messagges component
 const ChatMessage = ({ role, content, isLoading, src }: ChatMessageProps) => {
   const { toast } = useToast();
   const { theme } = useTheme();
@@ -33,12 +34,15 @@ const ChatMessage = ({ role, content, isLoading, src }: ChatMessageProps) => {
 
   return (
     <div
+      // If the role is user, the message will be aligned to the right, if not, it will be aligned to the left
       className={cn(
         "group flex items-start gap-x-3 py-4 w-full",
         role === "user" && "justify-end"
       )}
     >
+      {/* If the role is the system display bot avatar */}
       {role !== "user" && src && <BotAvatar src={src} />}
+      {/* Display the content of the messages */}
       <div className="rounded-md px-4 py-2 max-w-sm bg-primary/10">
         {isLoading ? (
           <BeatLoader size={5} color={theme === "light" ? "black" : "white"} />
@@ -46,7 +50,9 @@ const ChatMessage = ({ role, content, isLoading, src }: ChatMessageProps) => {
           content
         )}
       </div>
+      {/* If it is the user, display user avatar */}
       {role === "user" && <UserAvatar />}
+      {/* If the message is from the bot and is not loading display the copy button */}
       {role !== "user" && !isLoading && (
         <Button
           onClick={onCopy}
--- ai-comp/app/(chat)/(routes)/chat/[chatId]/components/chat-messages.tsx
+++ ai-comp/app/(chat)/(routes)/chat/[chatId]/components/chat-messages.tsx
@@ -2,6 +2,7 @@
 
 import { Companion } from "@prisma/client";
 import ChatMessage, { ChatMessageProps } from "./chat-message";
+import { ElementRef, useEffect, useRef, useState } from "react";
 
 interface ChatMessagesProps {
   companion: Companion;
@@ -14,17 +15,47 @@ const ChatMessages = ({
   isLoading,
   messages = [],
 }: ChatMessagesProps) => {
+  // Scrolling function to slip to top or bottom
+  const scrollRef = useRef<ElementRef<"div">>(null);
+
+  const [fakeLoading, setFakeLoading] = useState(
+    messages.length === 0 ? true : false
+  );
+
+  // Fake loading effect for the first message
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setFakeLoading(false);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+
+  // Scroll effect changes everytime there as messages increase
+  useEffect(() => {
+    scrollRef?.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages.length]);
+
   return (
     <div className="flex-1 overflow-y-auto pr-4">
       <ChatMessage
+        isLoading={fakeLoading}
         src={companion.src}
         role="system"
         content={`Hello, I am ${companion.name}, ${companion.description}`}
       />
-      <ChatMessage
-        role="user"
-        content={`Hello, I am ${companion.name}, ${companion.description}`}
-      />
+      {messages.map((message) => (
+        <ChatMessage
+          key={message.content}
+          role={message.role}
+          content={message.content}
+          src={message.src}
+        />
+      ))}
+      {isLoading && <ChatMessage role="system" src={companion.src} isLoading />}
+      <div ref={scrollRef} />
     </div>
   );
 };
