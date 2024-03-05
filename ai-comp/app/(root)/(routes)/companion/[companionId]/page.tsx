d52d7afeab86e74d322b9d07895d2c357177732a
rorense <rkorense@gmail.com>
2024-03-05 20:08:52 +1300

    fixed security issue

--- ai-comp/app/(root)/(routes)/companion/[companionId]/page.tsx
+++ ai-comp/app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -1,14 +1,22 @@
 import prismadb from "@/lib/prismadb";
 import CompanionForm from "./components/companion-form";
+import { auth, redirectToSignIn } from "@clerk/nextjs";
 
 interface CompanionPageProps {
   params: { companionId: string };
 }
 
 const CompanionPage = async ({ params }: CompanionPageProps) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirectToSignIn();
+  }
+
   const companion = await prismadb.companion.findUnique({
     where: {
       id: params.companionId,
+      userId,
     },
   });
 
--- ai-comp/app/api/companion/[companionId]/route.ts
+++ ai-comp/app/api/companion/[companionId]/route.ts
@@ -31,6 +31,7 @@ export async function PATCH(
     const companion = await prismadb.companion.update({
       where: {
         id: params.companionId,
+        userId: user.id,
       },
       data: {
         categoryId,
